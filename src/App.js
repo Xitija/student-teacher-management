4adb971816446505175ab7a8d85e5020ff254bb7
Kshitija Kadam <kshitijakadam04@gmail.com>
2024-06-02 01:59:56 +0530

    feat : added all pages to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
 import './App.css';
 import StudentView from './components/StudentView';
+import ClassView from './components/ClassView';
+import SchoolView from './components/SchoolView';
+import StudentDetail from './features/students/StudentDetail';
+import StudentForm from './features/students/StudentForm';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <StudentView />
-      </BrowserRouter>
-    </div>
-  );
-}
+    <div className='App'>
+      <Router>
+        <div>
+          <div className='navbar'>
+            <div className='logo'>Student Management System</div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to='/'>Students</Link>
+                </li>
+                <li>
+                  <Link to='/classes'>Classes</Link>
+                </li>
+                <li>
+                  <Link to='/school'>School</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
 
-export default App;
+          <Routes>
+            <Route path='/school' element={<SchoolView />} />
+            <Route path='/classes' element={<ClassView />} />
+            <Route path='/' element={<StudentView />} />
+            <Route path='/students/:id' element={<StudentDetail />} />
+            <Route path='/students/add' element={<StudentForm />} />
+            <Route path='/students/edit/:id' element={<StudentForm />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  )
+}
