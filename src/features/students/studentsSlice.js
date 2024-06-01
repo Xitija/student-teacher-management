17f439cbc131311b58b75d31ef08c3876e10c7b3
Kshitija Kadam <kshitijakadam04@gmail.com>
2024-06-02 01:59:37 +0530

    chore : added functions that are required

--- src/features/students/studentsSlice.js
+++ src/features/students/studentsSlice.js
@@ -11,16 +11,44 @@ export const fetchStudents = createAsyncThunk('students/fetchStudents', async ()
     return response.data;
 })
 
+export const addStudentAsync = createAsyncThunk('students/addStudentAsync', async (newStudent) => {
+    const response = await axios.post(HOST_URL + `/students`, newStudent)
+    return response.data;
+})
+
+export const updateStudentAsync = createAsyncThunk(
+    'students/updateStudentAsync', async ({ id, updatedStudent }) => {
+        const response = await axios.put(HOST_URL + `/students/${id}`, updatedStudent);
+        return response.data;
+    }
+)
+
+export const deleteStudentAsync = createAsyncThunk(
+    'students/deleteStudentAsync', async (id) => {
+        const response = await axios.delete(HOST_URL + `/students/${id}`);
+        return response.data;
+    }
+)
+
 const initialState = {
     students: [],
     status: 'idle',
-    error: null
+    error: null,
+    filter: "All",
+    sortBy: "name"
 }
 
 export const studentsSlice = createSlice({
     name: 'students',
     initialState,
-    reducers: {},
+    reducers: {
+        setFilter: (state, action) => {
+            state.filter = action.payload;
+        },
+        setSortBy: (state, action) => {
+            state.sortBy = action.payload
+        }
+    },
     extraReducers: (builder) => {
         builder
             .addCase(fetchStudents.pending, (state) => {
@@ -34,7 +62,48 @@ export const studentsSlice = createSlice({
             .addCase(fetchStudents.rejected, (state, action) => {
                 state.status = 'error';
                 state.error = action.error.message;
-            });
+            })
+            .addCase(addStudentAsync.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(addStudentAsync.fulfilled, (state, action) => {
+                state.status = 'success';
+                state.students.push(action.payload);
+            })
+            .addCase(addStudentAsync.rejected, (state, action) => {
+                state.status = 'error';
+                state.error = action.error.message;
+            })
+            .addCase(updateStudentAsync.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(updateStudentAsync.fulfilled, (state, action) => {
+                state.status = 'success';
+                const updatedStudent = action.payload;
+                console.log(updatedStudent,
+                    "updatedState"
+                )
+                const index = state.students.findIndex((s) => s.id === updatedStudent._id)
+                if (index !== -1) {
+                    state.students[index] = updatedStudent;
+                }
+            })
+            .addCase(updateStudentAsync.rejected, (state, action) => {
+                state.status = 'error';
+                state.error = action.error.message
+            })
+            .addCase(deleteStudentAsync.pending, (state) => {
+                state.status = 'loading'
+            })
+            .addCase(deleteStudentAsync.fulfilled, (state, action) => {
+                console.log(state.students , action ,"ookk")
+                state.status = 'success';
+                state.students = state.students.filter((student) => student._id !== action.payload.student._id)
+            })
+            .addCase(deleteStudentAsync.rejected, (state, action) => {
+                state.status = 'error';
+                state.error = action.error.message;
+            })
         // [fetchStudents.pending]: (state) => {
         //     state.status = "loading"
         // },
@@ -49,3 +118,7 @@ export const studentsSlice = createSlice({
         // }
     }
 })
+
+export const { setFilter, setSortBy } = studentsSlice.actions;
+
+export default studentsSlice.reducer;
