2672a381abc759dd201842f12cae54b39839015c
Kshitija Kadam <65657373+Xitija@users.noreply.github.com>
2025-03-29 20:58:22 +0530

    feat : added nav bar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,37 @@
+import { NavLink } from 'react-router-dom';
+import home from '../images/home.svg';
+
+
+const NavBar = () => {
+  const getActiveStyle = ({ isActive }) => ({
+    color: isActive ? '#003366' : ''
+  });
+
+  return (
+    <div
+      className="flex p-4 text-xl font-semibold justify-between"
+      style={{ background: '#00c5b4', color: '#ffffff' }}
+    >
+      <NavLink className='w-7' style={getActiveStyle} to="/">
+      <img src={home}  alt="home" />
+
+      </NavLink>
+      <div className="flex flex-row justify-end gap-3">
+        <NavLink style={getActiveStyle} to="/school">
+          School
+        </NavLink>
+        <NavLink style={getActiveStyle} to="/students">
+          Students
+        </NavLink>
+        <NavLink style={getActiveStyle} to="/classes">
+          Classes
+        </NavLink>
+        <NavLink style={getActiveStyle} to="/teachers">
+          Teachers
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
