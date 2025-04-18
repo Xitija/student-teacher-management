242273a130f5745961a51dbc3acd17e4e10f07ac
Kshitija Kadam <65657373+Xitija@users.noreply.github.com>
2025-04-18 18:00:53 +0530

    feat: enhance SchoolView layout with images and improved statistics display

--- src/components/SchoolView.jsx
+++ src/components/SchoolView.jsx
@@ -5,6 +5,8 @@ import {
   updateSchoolStats
 } from '../features/school/schoolSlice';
 import { fetchStudents } from '../features/students/studentsSlice';
+import pic31 from '../images/pic31.png';
+import pic32 from '../images/pic32.png';
 
 const SchoolView = () => {
   const schoolStats = useSelector((state) => state.school);
@@ -47,15 +49,25 @@ const SchoolView = () => {
   }, [students, dispatch]);
 
   return (
-    <div>
-      <h1>School View</h1>
-      <p>Total Students: {schoolStats.totalStudents}</p>
-      <p>Average Attendance: {schoolStats.averageAttendance.toFixed(2)}</p>
-      <p>Average Marks: {schoolStats.averageMarks.toFixed(2)}</p>
-      <p>
-        Top Student:{' '}
-        {schoolStats.topStudent ? schoolStats.topStudent.name : '-'}
-      </p>
+    <div className="flex flex-wrap w-full h-full">
+      <div className="w-full md:w-1/3">
+        <img className="w-full" src={pic31} alt="School" />
+      </div>
+      <div className="w-full md:w-1/3 p-2 flex flex-col justify-center items-center space-y-5 bg-white">
+        <h1 style={{ color: "#e14141"}} className="text-4xl font-bold underline">School Statistics</h1>
+        <div className="text-2xl ">
+          <p>Total Students: <span className='font-semibold'>{schoolStats.totalStudents}</span></p>
+          <p>Average Attendance: <span className='font-semibold'>{schoolStats.averageAttendance.toFixed(2)}</span></p>
+          <p>Average Marks: <span className='font-semibold'>{schoolStats.averageMarks.toFixed(2)}</span></p>
+        </div>
+        <p className="text-2xl font-semibold">
+          Top Student:{' '}
+          {schoolStats.topStudent ? schoolStats.topStudent.name : '-'}
+        </p>
+      </div>
+      <div className="w-full md:w-1/3 self-end">
+        <img className="w-full" src={pic32} alt="School" />
+      </div>
     </div>
   );
 };
